ea0897db2e488004d9ecf9bb79aff1a9c696b0c4
unknown <adnjoo@gmail.com>
2025-01-31 09:30:10 -0600

    GD

--- brackeys/scripts/player.gd
+++ brackeys/scripts/player.gd
@@ -3,12 +3,14 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -350.0
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add gravity.
 	if not is_on_floor():
-		velocity.y += get_gravity().y * delta
+		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -22,7 +24,17 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-		
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	# Apply movement	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
