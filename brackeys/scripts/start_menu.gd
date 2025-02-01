31c7ebfad844c1d703df8a496a111faaede08817
unknown <adnjoo@gmail.com>
2025-02-01 08:05:46 -0600

    sm

--- brackeys/scripts/start_menu.gd
+++ brackeys/scripts/start_menu.gd
@@ -2,13 +2,24 @@ extends Node2D
 
 @export var level_1: String = "res://levels/level_1.tscn"
 @onready var start_button = $Panel/VBoxContainer/StartButton  # Adjusted path to the StartButton
+@onready var about_link = $Panel/VBoxContainer/AboutLink  # Adjusted path to the StartButton
+@onready var exit_button = $Panel/VBoxContainer/ExitButton  # Adjusted path to the StartButton
 
 func _ready():
 	# Connect the button's "pressed" signal to a function
 	start_button.pressed.connect(self._on_start_button_pressed)
+	about_link.pressed.connect(self._on_about_link_pressed)
+	exit_button.pressed.connect(self._on_exit_button_pressed)
 
 # Called when the start button is pressed
 func _on_start_button_pressed():
 	LivesUI.visible = true
 	# Change the scene to Level 1
 	get_tree().change_scene_to_file(level_1)
+
+func _on_about_link_pressed() -> void:
+	var url = "https://www.heybam.boo"  # Replace with the URL you want to open
+	OS.shell_open(url)
+	
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()  # Quit the game
